1cbbcf0257466b506edf00181107ccc1d49af3ec
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-05-16 11:22:08 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
